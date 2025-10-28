5a0f74009bd9673a97facfa71a68da4e3f6fb600
AvaCook <avalaurelcook@gmail.com>
2025-10-28 16:18:03 -0400

    working: backend sends data to index.html

--- src/frontend/frontendServer.js
+++ src/frontend/frontendServer.js
@@ -1,15 +1,13 @@
-
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
-
 const app = express();
-const FRONTEND_PORT = 5500;
+const PORT = 5500;
 
-// Serve static files from frontend folder
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'frontend')));
+// Serve all files in "pages" (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, 'pages')));
 
-app.listen(FRONTEND_PORT, () => {
-  console.log(`Frontend running on http://localhost:${FRONTEND_PORT}`);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'index.html'));
 });
+
+app.listen(PORT, () => console.log(`Frontend running at http://localhost:${PORT}`));
